07e3f1c45fcbb55f7eb45144f00588510b1ec70f
Hadden <haddenlebaron@gmail.com>
2025-12-06 09:34:40 -0700

    sync tasks from todoist

--- main.py
+++ main.py
@@ -1,35 +1,42 @@
-from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton, QStackedWidget
+from PySide6.QtCore import Qt, QTimer
+from PySide6.QtWidgets import (QApplication, QWidget, QVBoxLayout, QLabel, QPushButton, QStackedWidget,
+                               QTableWidget, QTableWidgetItem, QCheckBox, QSizePolicy, QHBoxLayout)
+from todoist_api_python.api import TodoistAPI
 
 
 app = QApplication([])
 window = QWidget()
-layout = QVBoxLayout()
+main_layout = QVBoxLayout()
 stack = QStackedWidget()
 
-#
-# label = QLabel("To Do")
-# layout.addWidget(label)
-#
-# btn = QPushButton("Tap me")
-# layout.addWidget(btn)
-#
-# def on_click():
-#     print("Button pressed!")
-#
-# btn.clicked.connect(on_click)
+api = TodoistAPI("afd09492def59fe7e927e7e5f1ac6edb25b165dc")
+project_id = '6fHmMp9vx55g7fmf'
 
-def make_page(title, button_text, button_action):
+to_do_list = []
+
+try:
+    package = api.get_tasks(project_id=project_id)
+
+    for p in package:
+        for task in p:
+            to_do_list.append((task.content, task.is_completed))
+except Exception as e:
+    print("Error:", e)
+
+
+def make_page(title, button_text, button_action, qtable=None):
     """Utility to create a simple page."""
     page = QWidget()
     layout = QVBoxLayout(page)
     label = QLabel(title)
-    label.setAlignment(Qt.AlignCenter)
+    # label.setAlignment(Qt.AlignCenter)
     label.setStyleSheet("font-size: 32px;")
     button = QPushButton(button_text)
-    # button.setStyleSheet("font-size: 24px; padding: 10px;")
+    button.setStyleSheet("font-size: 24px; padding: 10px;")
     button.clicked.connect(button_action)
     layout.addWidget(label)
+    if qtable:
+        layout.addWidget(qtable)
     layout.addWidget(button)
     return page
 
@@ -39,7 +46,47 @@ def show_completed():
 def show_todo():
     stack.setCurrentIndex(0)
 
-todo_list = make_page('To-Do list', 'Go to Completed', show_completed)
+# data = ["dishes", "floors", "trash", "fridge", "wipe counters", "make dinner", "check mail", "walk dog", "change sheets", "laundry"]
+table = QTableWidget(len(to_do_list), 2)
+table.setStyleSheet("""
+                    QTableWidget {font-size: 24px;}
+                    QTableWidget::item {
+                        padding-left: 7px;
+                    }""")
+
+table.setColumnWidth(0, 50)
+table.setColumnWidth(1, 696)
+
+table.verticalHeader().setVisible(False)
+table.horizontalHeader().setVisible(False)
+
+for i in range(len(to_do_list)):
+    checkbox = QCheckBox()
+    checkbox.setStyleSheet("""
+    QCheckBox::indicator {
+        width: 40px;
+        height: 40px;
+        image: url(unchecked.png)
+    }
+    QCheckBox::indicator:checked {
+        image: url(checkmark.png);
+    }
+    
+    """)
+    checkbox_widget = QWidget()
+    checkbox_widget.setMinimumSize(30, 30)
+
+    layout = QHBoxLayout(checkbox_widget)
+    layout.addWidget(checkbox)
+    layout.setAlignment(Qt.AlignCenter)
+    layout.setContentsMargins(0, 0, 0, 0)
+    table.setRowHeight(i, 50)
+    table.setCellWidget(i, 0, checkbox_widget)
+    table.setItem(i, 1, QTableWidgetItem(to_do_list[i][0]))
+
+
+
+todo_list = make_page('To-Do list', 'Go to Completed', show_completed, table)
 completed_list = make_page('Completed Tasks', 'Back to Todo', show_todo)
 
 
@@ -47,15 +94,14 @@ stack.addWidget(todo_list)
 stack.addWidget(completed_list)
 stack.setCurrentIndex(0)
 
-
-layout.addWidget(stack)
-window.setLayout(layout)
+main_layout.addWidget(stack)
+window.setLayout(main_layout)
+window.resize(800, 480)
 window.show()
 
-# window.setWindowFlags(Qt.FramelessWindowHint)
-# window.showFullScreen()
 
 app.exec()
 
-
-
+# possibly helpful when actually on raspberry pi
+# window.setWindowFlags(Qt.FramelessWindowHint)
+# window.showFullScreen()
